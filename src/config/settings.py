ad20e58636548f53758b55d96ffbf3cd751ca329
gre-ilya <ilya.grebenyuk.01@mail.ru>
2023-03-07 13:54:47 +0500

    moved to postgresql

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-06&&*q-i#ym#au_ie$lqxwj^g)l7xy9mj=##5lbp5$+4g632gp"
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,7 +76,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['PSQL_DATABASE'],
+        'USER': os.environ['PSQL_USER'],
+        'PASSWORD': os.environ['PSQL_PASS'],
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
